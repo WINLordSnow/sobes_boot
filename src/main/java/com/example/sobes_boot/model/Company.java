40e4d962e1918597aa4ec3488ed6ebecee693599
WINLordSnow <69006937+WINLordSnow@users.noreply.github.com>
2021-06-21 21:42:22 +0400

    89

--- src/main/java/com/example/sobes_boot/model/Company.java
+++ src/main/java/com/example/sobes_boot/model/Company.java
@@ -4,13 +4,15 @@ import javax.persistence.*;
 import java.util.List;
 
 @Entity
+@Access(AccessType.FIELD)
 public class Company {
-
     @Id
     @GeneratedValue
+
     private Integer id;
 
-    @Column(name = "final")
+   // @Column(name = "final")
+
     private final String name="6";
 
     private String address;
@@ -28,6 +30,17 @@ public class Company {
 
     public Sex getSex() {
         return sex;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
     }
 
     public void setSex(Sex sex) {
